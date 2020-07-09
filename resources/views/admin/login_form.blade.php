b91f6dc063cb90ca90d230424ed7948353b5859d
mostakimul1994 <mostakimul.1994@gmail.com>
2020-07-09 17:33:08 +0600

    User Authentication half Done

---
+++ resources/views/admin/login_form.blade.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<!-- Required meta tags -->
+	@include('layouts.admin._head')
+</head>
+
+<body>
+	<div class="container-scroller">
+		<div class="container-fluid page-body-wrapper full-page-wrapper">
+			<div class="content-wrapper d-flex align-items-stretch auth auth-img-bg">
+				<div class="row flex-grow">
+					<div class="col-lg-6 d-flex align-items-center justify-content-center">
+						<div class="auth-form-transparent text-left p-3">
+							<div class="brand-logo">
+								<img src="{{ asset ('/images/logo.svg')}}" alt="logo">
+							</div>
+							<h4>Welcome back!</h4>
+							<h6 class="font-weight-light">Happy to see you again!</h6>
+							<form class="pt-3" action="{{asset('login')}}" method="post">
+								@csrf
+								<div class="form-group">
+									<label for="email">Email</label>
+									<div class="input-group">
+										<div class="input-group-prepend bg-transparent">
+											<span class="input-group-text bg-transparent border-right-0">
+												<i class="ti-user text-primary"></i>
+											</span>
+										</div>
+										<input type="email" name="email" class="form-control form-control-lg border-left-0" id="email" placeholder="Email">
+									</div>
+								</div>
+								<div class="form-group">
+									<label for="password">Password</label>
+									<div class="input-group">
+										<div class="input-group-prepend bg-transparent">
+											<span class="input-group-text bg-transparent border-right-0">
+												<i class="ti-lock text-primary"></i>
+											</span>
+										</div>
+										<input type="password" name="password" class="form-control form-control-lg border-left-0" id="password" placeholder="Password">                        
+									</div>
+								</div>
+								<div class="my-2 d-flex justify-content-between align-items-center">
+									<div class="form-check">
+										<label class="form-check-label text-muted">
+											<input type="checkbox" class="form-check-input">
+											Keep me signed in
+										</label>
+									</div>
+									<a href="#" class="auth-link text-black">Forgot password?</a>
+								</div>
+
+								<div class="my-3">
+
+									<button type="submit" class="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn">LOGIN</button>
+								</div>
+
+
+								<div class="text-center mt-4 font-weight-light">
+									Don't have an account? <a href="register-2.html" class="text-primary">Create</a>
+								</div>
+							</form>
+						</div>
+					</div>
+					<div class="col-lg-6 login-half-bg d-flex flex-row">
+						<p class="text-white font-weight-medium text-center flex-grow align-self-end">Copyright &copy; 2020  All rights reserved.</p>
+					</div>
+				</div>
+			</div>
+			<!-- content-wrapper ends -->
+		</div>
+		<!-- page-body-wrapper ends -->
+	</div>
+
+	@include('layouts.admin._scripts')
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,10 @@ Route::get('/', function () {
 
 });
 Route::get('dashboard','DashboardController@dashboard')->name('admin.dashboard');
+
+Route::get('login','LoginController@index')->name('user.login');
+Route::post('login','LoginController@login')->name('login');
+
 Route::resource('post','PostController');
 Route::resource('user','UserController');
 
