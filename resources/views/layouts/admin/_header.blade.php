012b0479b43d2cb198976b255496d3191ce05ccd
mostakimul1994 <mostakimul.1994@gmail.com>
2020-07-10 22:55:38 +0600

    authentication deleted

--- resources/views/layouts/admin/_header.blade.php
+++ resources/views/layouts/admin/_header.blade.php
@@ -121,10 +121,10 @@
                 <i class="ti-settings text-primary"></i>
                 Settings
               </a>
-              <a class="dropdown-item">
-                <i class="ti-power-off text-primary"></i>
-                Logout
-              </a>
+              <form action="" class="" method="post">
+                @csrf
+                <button type="" class="ti-power-off text-primary">Logout</button>
+              </form>
             </div>
           </li>
         </ul>
--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,11 @@ Route::get('/', function () {
     return view('welcome');
 
 });
+
+
 Route::get('dashboard','DashboardController@dashboard')->name('admin.dashboard');
 
-Route::get('login','LoginController@index')->name('user.login');
-Route::post('login','LoginController@login')->name('login');
+
 
 Route::resource('post','PostController');
 Route::resource('user','UserController');
