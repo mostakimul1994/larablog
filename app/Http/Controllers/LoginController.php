561de144e2880f81220f90c1a680a95db1eaaed5
mostakimul1994 <mostakimul.1994@gmail.com>
2020-07-10 23:39:52 +0600

    auth not done

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\support\Facades\Auth;
+use Illuminate\Http\File;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+	public function index(){
+		return view('admin.login_form');
+	}
+
+	public function login(Request $request){
+
+		if (Auth::attempt(['email' => $request->email,'password' => $request->password])) {
+
+			return redirect()->route('admin.dashboard');
+		}
+		return redirect()->back()->withInput($request->all());	
+	}
+
+}
--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,8 @@ Route::get('/', function () {
 
 Route::get('dashboard','DashboardController@dashboard')->name('admin.dashboard');
 
-
+Route::get('login','LoginController@index')->name('user.login');
+Route::post('login','LoginController@login')->name('login');
 
 Route::resource('post','PostController');
 Route::resource('user','UserController');
